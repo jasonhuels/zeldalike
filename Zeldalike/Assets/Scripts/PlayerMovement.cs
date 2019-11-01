3180688ebf316947d4acb5be2f9a32c0c113fa39
Jason Huels <jasonhuels@yahoo.com>
2019-11-01 13:01:54 -0700

    Initial commit

---
+++ Zeldalike/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public float speed;
+	private Rigidbody2D myRigidbody;
+	private Vector3 change;
+
+	// Use this for initialization
+	void Start () {
+		myRigidbody = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		change = Vector3.zero;
+		change.x = Input.GetAxis ("Horizontal");
+		change.y = Input.GetAxis ("Vertical");
+		if(change != Vector3.zero)
+		{
+			MoveCharacter();
+		}
+	}
+
+	void MoveCharacter()
+	{
+		myRigidbody.MovePosition(transform.position + change * speed * Time.deltaTime);
+	}
+}
