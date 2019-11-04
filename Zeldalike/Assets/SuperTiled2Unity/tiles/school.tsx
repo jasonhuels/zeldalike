d1cccb0238fb9273dc540106d5ddb7b08c2a51cd
Jason Huels <jasonhuels@yahoo.com>
2019-11-04 09:47:43 -0800

    add tiles to project

---
+++ Zeldalike/Assets/SuperTiled2Unity/tiles/school.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.5" name="school" tilewidth="32" tileheight="32" tilecount="119" columns="17">
+ <image source="../../Art/gfx/Game Boy Advance - The Legend of Zelda The Minish Cap - Funday School.png" width="565" height="240"/>
+</tileset>
