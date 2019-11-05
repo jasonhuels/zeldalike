01470ed615999b7700fcf86f2d254e938ec3ae81
Jennifer Batara <jennifer.batara.dev@gmail.com>
2019-11-05 11:50:40 -0800

    Merge PushBlock.cs from jen branch

--- Zeldalike/Assets/Scripts/PushBlock.cs
+++ Zeldalike/Assets/Scripts/PushBlock.cs
@@ -4,30 +4,35 @@ using UnityEngine;
 
 public class PushBlock : MonoBehaviour
 {
-    public string value;
-    //public GameObject player = GameObject.Find("Player");
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+  ParticleSystem dustParticles;
+  public string value;
+  //public GameObject player = GameObject.Find("Player");
+  // Start is called before the first frame update
+  void Start()
+  {
+    dustParticles = this.GetComponent<ParticleSystem>();
+    var em = dustParticles.emission;
+    em.enabled = false;
+  }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+  // Update is called once per frame
+  void Update()
+  {
 
-    private void OnCollisionEnter(Collision other)
+  }
+
+  private void OnCollisionEnter(Collision other)
+  {
+    Debug.Log("touching");
+    if (other.gameObject.tag == "Player")
     {
-        Debug.Log("touching");
-        if (other.gameObject.tag == "Player")
-        {
-            Debug.Log("touching");
-        } else {
-            Debug.Log("!touching");
-        }
+      Debug.Log("touching");
     }
+    else
+    {
+      Debug.Log("!touching");
+    }
+  }
 }
 
 
