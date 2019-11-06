e7d897559a4999060749808700a85a2fd48ca3c6
Jennifer Batara <jennifer.batara.dev@gmail.com>
2019-11-06 15:03:27 -0800

    Enemy Particles WIP

--- Zeldalike/Assets/Scripts/Enemy.cs
+++ Zeldalike/Assets/Scripts/Enemy.cs
@@ -4,41 +4,86 @@ using UnityEngine;
 
 public enum EnemyState
 {
-    idle,
-    walk,
-    attack,
-    stagger
+  idle,
+  walk,
+  attack,
+  stagger
 }
 
 public class Enemy : MonoBehaviour
 {
-    public EnemyState currentState;
-    public int health;
-    public string enemyName;
-    public int baseAttack;
-    public float moveSpeed;
-    public bool dying;
+  public EnemyState currentState;
+  public int health;
+  public string enemyName;
+  public int baseAttack;
+  public float moveSpeed;
+  public bool dying;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        dying = false;
-    }
+  // Start is called before the first frame update
+  void Start()
+  {
+    dying = false;
+    //SphereCollider sc = gameObject.AddComponent(typeof(SphereCollider)) as SphereCollider;
+    
+  }
 
-    // Update is called once per frame
-    void Update()
+  // Update is called once per frame
+  void Update()
+  {
+    if (dying)
     {
-        if(dying)
-        {
-            StartCoroutine(DieCo());
-        }
+      StartCoroutine(DieCo());
     }
+  }
 
-    private IEnumerator DieCo()
-    {
-        transform.rotation = Quaternion.Euler(0, 0, Random.value*360);
-        yield return new WaitForSeconds(1);
-        //StartCoroutine(DieCo());
-        Destroy(this.gameObject);
-    }
-}
+  private IEnumerator DieCo()
+  {
+    transform.rotation = Quaternion.Euler(0, 0, Random.value * 360);
+    yield return new WaitForSeconds(1);
+    //StartCoroutine(DieCo());
+    Destroy(this.gameObject);
+  }
+
+  public ParticleSystem CreateEnemyDeathParticleSystem(GameObject go)
+  {
+    ParticleSystem ps = go.AddComponent(typeof(ParticleSystem)) as ParticleSystem;
+
+    //general
+    var main = ps.main;
+    
+    //set colors
+    var startColor = main.startColor;
+    startColor.colorMax = Color.red;
+    startColor.colorMin = Color.black;
+    main.startColor = startColor;
+
+    main.duration = 0.5f;
+    main.startLifetime = 0.5f;
+    main.startSpeed = 5.0f;
+    
+    var startSize = main.startSize;
+    startSize.constantMax = 0.1f;
+    startSize.constantMin = 0.05f;
+   
+   //emission
+    var em = ps.emission;
+    em.enabled = true;
+    em.rateOverTime = 50f;
+
+    //size
+    var sz = ps.sizeOverLifetime;
+    sz.enabled = true;
+    AnimationCurve curve = new AnimationCurve();
+    curve.AddKey(1.0f, 0.0f);
+    curve.AddKey(0.0f, 1.0f); 
+
+    //shape
+    var sh = ps.shape;
+    sh.enabled = true;
+    sh.shapeType = ParticleSystemShapeType.Sphere;
+    sh.radius = 1;
+
+    
+
+    return ps;
+  }
