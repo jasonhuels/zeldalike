442e006a601f5c614a3c161660fa314c0c9dee78
Jennifer Batara <jennifer.batara.dev@gmail.com>
2019-11-05 16:20:35 -0800

    Merge Assets folder from jasonsBranch branch

--- Zeldalike/Assets/Scripts/ButtonCorrect.cs
+++ Zeldalike/Assets/Scripts/ButtonCorrect.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class ButtonCorrect : MonoBehaviour
 {
     public string value;
+    public bool correct;
     // Start is called before the first frame update
     void Start()
     {
@@ -29,17 +30,18 @@ public class ButtonCorrect : MonoBehaviour
     {
         if (other.gameObject.tag == "Pushable" && other.GetComponent<PushBlock>().value == value)
         {
-            Debug.Log("You are the best coder ever!!");
+            //Debug.Log("You are the best coder ever!!");
             //change box to blue
             other.GetComponent<SpriteRenderer>().color = Color.blue;
             //enable particle emissions
-            var em = other.GetComponent<ParticleSystem>().emission;
-            em.enabled = true;
+            //var em = other.GetComponent<ParticleSystem>().emission;
+            //em.enabled = true;
             //snap box into trigger place
             Vector3 v3 = this.transform.position - other.transform.position;
             other.transform.position += v3;
             Destroy(other.GetComponent<FixedJoint2D>());
             Destroy(other.GetComponent<Rigidbody2D>());
+            correct = true;
         }
     }
 }
--- Zeldalike/Assets/TiledMaps/Text.tsx
+++ Zeldalike/Assets/TiledMaps/Text.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.2.5" name="Text" tilewidth="16" tileheight="16" tilecount="759" columns="30">
+<tileset version="1.2" tiledversion="1.2.5" name="Text" tilewidth="16" tileheight="16" tilecount="540" columns="30">
  <image source="../Art/gfx/font2.png" width="480" height="288"/>
 </tileset>
