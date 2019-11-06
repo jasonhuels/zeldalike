482622a6aa522c97d5abe8ed3191b52bcdfdb5c7
Jason Huels <jasonhuels@yahoo.com>
2019-11-06 14:12:16 -0800

    readd combat door

---
+++ Zeldalike/Assets/Scripts/CombatDoor.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CombatDoor : MonoBehaviour
+{
+    //private bool openDoor;
+    public GameObject target;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //openDoor = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        target = GameObject.FindWithTag("Enemy");
+        Debug.Log(target);
+        if (target == null)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+}
